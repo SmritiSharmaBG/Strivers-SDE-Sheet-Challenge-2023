e8834571e55adc1af519c9f9d252d216dde00855
Smriti Sharma <77639024+SmritiSharmaBG@users.noreply.github.com>
2023-06-04 15:32:33 +0530

    Added Code

--- Recursion/SubsetSums.java
+++ Recursion/SubsetSums.java
@@ -1,1 +1,22 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static ArrayList<Integer> subsetSum(int num[]) {
+        int n = num.length;
+        ArrayList<Integer> ans = new ArrayList<>();
+        rec(num, ans, 0, n, 0);
+        Collections.sort(ans);
+        return ans;
+    }
 
+    static void rec(int[] num, ArrayList<Integer> ans, int i, int n, int sum) {
+        if (i == n) {
+            ans.add(sum);
+            return;
+        }
+        // not take
+        rec(num, ans, i + 1, n, sum);
+        // take
+        rec(num, ans, i + 1, n, sum + num[i]);
+    }
+}
