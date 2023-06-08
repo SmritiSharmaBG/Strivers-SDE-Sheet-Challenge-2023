9bffb7361b7d9eda98cad6100e6c22de9f472b36
Smriti Sharma <77639024+SmritiSharmaBG@users.noreply.github.com>
2023-06-09 04:07:54 +0530

    Create PostOrderTraversal.java

---
+++ Binary Tree/PostOrderTraversal.java
@@ -0,0 +1,36 @@
+import java.util.* ;
+import java.io.*; 
+/*
+    
+    Following is the Binary Tree node structure:
+
+    public class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.data = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+           this.data = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+*/
+
+public class Solution {
+    public static List < Integer > getPostOrderTraversal(TreeNode root) {
+    	List<Integer> ds = new ArrayList<>();
+    	rec(ds, root);
+        return ds;
+    }
+
+    private static void rec(List<Integer> ds, TreeNode node) {
+        if (node == null) return;
+        
+		rec(ds, node.left);
+        rec(ds, node.right);
+        ds.add(node.data);
+    }
+}
